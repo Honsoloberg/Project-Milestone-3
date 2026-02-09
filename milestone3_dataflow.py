f1530b6440834d984da2d75eeccc6d7ab1be1a9b
Honsoloberg <117222684+Honsoloberg@users.noreply.github.com>
2026-02-09 12:51:56 -0500

    added comments to dataflow

--- milestone3_dataflow.py
+++ milestone3_dataflow.py
@@ -7,6 +7,7 @@ import apache_beam as beam
 from apache_beam.io import ReadFromPubSub, WriteToPubSub
 from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
 
+# Decodes the Pub/Sub message and parses the JSON data into a dict
 class DecodeParseJSON(beam.DoFn):
   def process(self, data):
     temp = data.decode('utf-8')
@@ -34,31 +35,39 @@ def run(argv=None, save_main_session=True):
 
   with beam.Pipeline(options=pipeline_options) as p:
 
+    # reads from the Google Topic as a subscription to ensure
+    # all messages are consumed, not just new ones.
     read = p | 'Read' >> ReadFromPubSub(subscription=known_args.input)
     
+    # Decode the Pub/Sub message and parse the JSON data into a dict
     decoded = read | 'parse' >> (beam.ParDo(DecodeParseJSON()))
     
     # Convert Units of Temp -> F and Pressure -> psi
     def convert_data(data):
+      # Check if the entries are there
       temp = data.get("temperature")
       pressure = data.get("pressure")
 
+      # If the entries exist, then convert them.
       if temp is not None:
         data["temperature"] = temp * (9/5) + 32
 
       if pressure is not None:
         data["pressure"] = pressure/6.895
 
+      # return converted data
       return data
 
+    # serialize the dictionary to JSON then binary
     def serialize(data):
       return json.dumps(data).encode('utf-8')
 
+    # process data. Filter null values then run convert fucntion
     process=(decoded 
         | 'filter' >> beam.Filter(lambda x: x is not None)
         | 'convert' >> beam.Map(convert_data))
     
-    # save to pubsub
+    # run serialize function then publish to the output topic
     output = process | 'serialize' >> beam.Map(serialize)
     output | 'publish' >> WriteToPubSub(topic=known_args.output)
 
